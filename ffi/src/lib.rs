0e38a3a4d46ba71e5d3ba2fa584311b76e3415c5
Emily Toop <etoop@mozilla.com>
2018-02-28 18:01:25 +0000

    Fixing threads

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -13,11 +13,14 @@ extern crate mentat;
 use std::collections::{
     BTreeSet,
 };
+use std::ops::Deref;
 use std::os::raw::{
     c_char,
     c_void,
 };
 use std::slice;
+use std::thread;
+use std::time;
 
 pub use mentat::{
     NamespacedKeyword,
@@ -54,12 +57,31 @@ pub struct ExternTxReportList {
     pub len: usize
 }
 
+#[derive(Debug)]
+struct CallbackWrapper(Callback);
+impl Deref for CallbackWrapper {
+    type Target = Callback;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Drop for CallbackWrapper {
+    fn drop(&mut self) {
+        (self.destroy)(self.obj);
+    }
+}
+
 #[repr(C)]
+#[derive(Debug)]
 pub struct Callback {
     pub obj: *mut c_void,
     pub destroy: extern fn(obj: *mut c_void),
-    pub callback_fn: extern fn(),
+    pub callback_fn: extern fn(obj: *mut c_void, key: *const c_char, reports: *mut ExternTxReportList),
 }
+
+unsafe impl Send for Callback {}
 
 #[no_mangle]
 pub extern "C" fn new_store(uri: *const c_char) -> *mut Store {
@@ -74,11 +96,11 @@ pub unsafe extern "C" fn store_destroy(store: *mut Store) {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn store_register_observer(store: *mut Store, key: *const c_char, attributes: *const i64, attributes_len: usize, callback: *mut Callback) {
+pub unsafe extern "C" fn store_register_observer(store: *mut Store, key: *const c_char, attributes: *const i64, attributes_len: usize, callback: Callback) {
     println!("Registering observer");
     let store = &mut*store;
     println!("got store");
-    let callback = &mut*callback;
+    let callback = CallbackWrapper(callback);
     println!("got callback");
     let mut attribute_set = BTreeSet::new();
     let slice = slice::from_raw_parts(attributes, attributes_len);
@@ -113,9 +135,7 @@ pub unsafe extern "C" fn store_register_observer(store: *mut Store, key: *const 
         };
         println!("reports {:?}", reports);
         println!("calling callback {:?}", callback.obj);
-
-        (callback.callback_fn)();
-        // (callback.callback_fn)(callback.obj, string_to_c_char(obs_key), Box::into_raw(Box::new(reports)));
+        (callback.callback_fn)(callback.obj, string_to_c_char(obs_key), Box::into_raw(Box::new(reports)));
         println!("callback called");
     });
     println!("TxObserver created");
