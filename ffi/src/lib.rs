2ee6dd6c7a73e0f5436f603d8deb8318ba08692a
Emily Toop <etoop@mozilla.com>
2018-02-28 18:42:52 +0000

    Fixing interating report lists from iOS

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -16,6 +16,7 @@ use std::collections::{
 use std::ops::Deref;
 use std::os::raw::{
     c_char,
+    c_int,
     c_void,
 };
 use std::slice;
@@ -37,14 +38,14 @@ pub use utils::strings::{
 };
 
 #[repr(C)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct AttributeList {
     pub attributes: Box<[i64]>,
     pub len: usize
 }
 
 #[repr(C)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ExternTxReport {
     pub txid: i64,
     pub changes: AttributeList,
@@ -97,24 +98,16 @@ pub unsafe extern "C" fn store_destroy(store: *mut Store) {
 
 #[no_mangle]
 pub unsafe extern "C" fn store_register_observer(store: *mut Store, key: *const c_char, attributes: *const i64, attributes_len: usize, callback: Callback) {
-    println!("Registering observer");
     let store = &mut*store;
-    println!("got store");
     let callback = CallbackWrapper(callback);
-    println!("got callback");
     let mut attribute_set = BTreeSet::new();
     let slice = slice::from_raw_parts(attributes, attributes_len);
-    println!("slice {:?}", slice);
     for i in 0..attributes_len {
         let attr = slice[i];
-        println!("adding {} to attribute set", attr);
         attribute_set.insert(attr);
     }
-    println!("Attribute set  {:?}", attribute_set);
     let key = c_char_to_string(key);
-    println!("Registering observer for {:?}", key);
     let tx_observer = TxObserver::new(attribute_set, move |obs_key, batch| {
-        println!("observer function called {:?}: {:?}", obs_key, batch);
         let extern_reports: Vec<ExternTxReport> = batch.iter().map(|report| {
             let changes: Vec<i64> = report.changeset.iter().map(|i|i.clone()).collect();
             let len = changes.len();
@@ -127,41 +120,44 @@ pub unsafe extern "C" fn store_register_observer(store: *mut Store, key: *const 
                 changes: changelist,
             }
         }).collect();
-        println!("ExternReports {:?}", extern_reports);
         let len = extern_reports.len();
         let reports = ExternTxReportList {
             reports: extern_reports.into_boxed_slice(),
             len: len,
         };
-        println!("reports {:?}", reports);
-        println!("calling callback {:?}", callback.obj);
         (callback.callback_fn)(callback.obj, string_to_c_char(obs_key), Box::into_raw(Box::new(reports)));
-        println!("callback called");
     });
-    println!("TxObserver created");
     store.register_observer(key, tx_observer);
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn store_unregister_observer(store: *mut Store, key: *const c_char) {
-    println!("Unregistering observer");
     let store = &mut*store;
     let key = c_char_to_string(key);
-    println!("Unregistering observer for {:?}", key);
     store.unregister_observer(&key);
-    println!("{:?} unregistered", key);
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn store_entid_for_attribute(store: *mut Store, attr: *const c_char) -> i64 {
-    println!("store_entid_for_attribute");
     let store = &mut*store;
     let attr_name = c_char_to_string(attr);
-    println!("attr_name {:?}", attr_name);
     let parts: Vec<&str> = attr_name.split("/").collect();
     let kw = NamespacedKeyword::new(parts[0], parts[1]);
-    println!("kw {:?}", kw);
     let entid = store.conn().current_schema().get_entid(&kw).unwrap();
-    println!("entid {:?}", entid);
     entid.into()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tx_report_list_entry_at(tx_report_list: *mut ExternTxReportList, index: c_int) -> *const ExternTxReport {
+    let tx_report_list = &*tx_report_list;
+    let index = index as usize;
+    let report = Box::new(tx_report_list.reports[index].clone());
+    Box::into_raw(report)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn changelist_entry_at(attribute_list: *mut AttributeList, index: c_int) -> i64 {
+    let attribute_list = &*attribute_list;
+    let index = index as usize;
+    attribute_list.attributes[index].clone()
 }
